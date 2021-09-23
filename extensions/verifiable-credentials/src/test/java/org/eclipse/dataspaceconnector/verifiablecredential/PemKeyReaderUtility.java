0d09ca8bc10eb5db3c802befdba77c7a283ce01e
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-09-23 08:46:38 +0200

    test fixes

--- extensions/verifiable-credentials/src/test/java/org/eclipse/dataspaceconnector/verifiablecredential/PemKeyReaderUtility.java
+++ extensions/verifiable-credentials/src/test/java/org/eclipse/dataspaceconnector/verifiablecredential/PemKeyReaderUtility.java
@@ -3,6 +3,7 @@ package org.eclipse.dataspaceconnector.verifiablecredential;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.jwk.ECKey;
 import com.nimbusds.jose.jwk.JWK;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -12,13 +13,14 @@ import java.nio.file.Path;
 /**
  * This is not an actual unit test, it is merely a utility to read pem files and get the parameters
  */
-public class PemKeyReaderTest {
+public class PemKeyReaderUtility {
 
     @Test
+    @Disabled
     void readPemFile() {
-        var jwk1 = parsePemAsJWK("/home/paul/dev/ion-demo/keys/consumer-pub.pem");
-        var jwk2 = parsePemAsJWK("/home/paul/dev/ion-demo/keys/provider-pub.pem");
-        var jwk3 = parsePemAsJWK("/home/paul/dev/ion-demo/keys/verifier-pub.pem");
+        var jwk1 = parsePemAsJWK("/home/paul/dev/ion-demo/keys2/public.pem");
+//        var jwk2 = parsePemAsJWK("/home/paul/dev/ion-demo/keys2/provider-pub.pem");
+//        var jwk3 = parsePemAsJWK("/home/paul/dev/ion-demo/keys2/verifier-pub.pem");
     }
 
     private JWK parsePemAsJWK(String resourceName) {
