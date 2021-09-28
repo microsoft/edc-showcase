80a72b667d76ab04f0e24de0e4d45a3be6a6165e
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-09-28 16:48:40 +0200

    added registration service

---
+++ launchers/registration-service/build.gradle.kts
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) Microsoft Corporation.
+ * All rights reserved.
+ */
+
+plugins {
+    `java-library`
+    application
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+}
+
+val edcversion: String by project
+val group = "org.eclipse.dataspaceconnector"
+
+dependencies {
+    // EDC core dependencies
+    implementation("${group}:core:${edcversion}")
+    implementation("${group}:in-mem.process-store:${edcversion}")
+
+    implementation("${group}:core.bootstrap:${edcversion}")
+    implementation("${group}:dataspaceconnector.common.util:${edcversion}")
+    implementation("${group}:core.protocol-web:${edcversion}")
+    implementation("${group}:azure.events-config:${edcversion}")
+    implementation("${group}:data-protocols.ion-client:${edcversion}")
+    implementation("${group}:iam.registration-service:${edcversion}")
+    implementation("${group}:iam.registration-service-api:${edcversion}")
+    implementation("${group}:in-mem.identity-hub:${edcversion}")
+    implementation("${group}:in-mem.did-document-store:${edcversion}")
+
+    // todo: replace this with a finer-grained dependency structure. we don't need VCs here!!
+    implementation("${group}:iam.verifiable-credentials:${edcversion}")
+
+}
+
+application {
+    @Suppress("DEPRECATION")
+    mainClassName = "com.microsoft.ion.demo.RegistrationServiceRuntime"
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    exclude("**/pom.properties", "**/pom.xm")
+    mergeServiceFiles()
+    archiveFileName.set("regsrv.jar")
+}
+
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "ion-demo"
 
 include(":launchers:connector")
 include(":launchers:junit")
+include(":launchers:registration-service")
 
 include(":extensions:dataseeding:hub")
 include(":extensions:dataseeding:catalog")
