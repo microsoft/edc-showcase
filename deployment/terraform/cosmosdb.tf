4e39271c06458f805489139e8c81c6676a6eb5a3
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2022-05-04 08:16:36 +0200

    added lease SPROC

--- deployment/terraform/cosmosdb.tf
+++ deployment/terraform/cosmosdb.tf
@@ -85,34 +85,6 @@ resource "azurerm_cosmosdb_sql_container" "provider-assetindex-container" {
   }
 }
 
-# Asset Index container for Connector3
-resource "azurerm_cosmosdb_sql_container" "connector3-assetindex-container" {
-  name                  = "connector3"
-  resource_group_name   = azurerm_cosmosdb_account.showcase-cosmos-account.resource_group_name
-  account_name          = azurerm_cosmosdb_account.showcase-cosmos-account.name
-  database_name         = azurerm_cosmosdb_sql_database.asset-index-db.name
-  partition_key_path    = "/${var.asset-index-partkey}"
-  partition_key_version = 1
-  throughput            = 400
-
-  indexing_policy {
-    indexing_mode = "Consistent"
-
-    included_path {
-      path = "/*"
-    }
-
-    included_path {
-      path = "/included/?"
-    }
-
-    excluded_path {
-      path = "/excluded/?"
-    }
-  }
-
-}
-
 
 # create database that contains all the contract-definition indexes
 resource "azurerm_cosmosdb_sql_database" "contractdefinition-store-db" {
@@ -175,34 +147,6 @@ resource "azurerm_cosmosdb_sql_container" "provider-contractdefstore-container" 
       path = "/excluded/?"
     }
   }
-}
-
-# ContractDefinition-Store container for Connector3
-resource "azurerm_cosmosdb_sql_container" "connector3-contractdefstore-container" {
-  name                  = "connector3"
-  resource_group_name   = azurerm_cosmosdb_account.showcase-cosmos-account.resource_group_name
-  account_name          = azurerm_cosmosdb_account.showcase-cosmos-account.name
-  database_name         = azurerm_cosmosdb_sql_database.contractdefinition-store-db.name
-  partition_key_path    = "/${var.asset-index-partkey}"
-  partition_key_version = 1
-  throughput            = 400
-
-  indexing_policy {
-    indexing_mode = "Consistent"
-
-    included_path {
-      path = "/*"
-    }
-
-    included_path {
-      path = "/included/?"
-    }
-
-    excluded_path {
-      path = "/excluded/?"
-    }
-  }
-
 }
 
 
@@ -269,33 +213,6 @@ resource "azurerm_cosmosdb_sql_container" "provider-contractnegotiation-containe
   }
 }
 
-# ContractDefinition-Store container for Connector3
-resource "azurerm_cosmosdb_sql_container" "connector3-contractnegotiation-container" {
-  name                  = "connector3"
-  resource_group_name   = azurerm_cosmosdb_account.showcase-cosmos-account.resource_group_name
-  account_name          = azurerm_cosmosdb_account.showcase-cosmos-account.name
-  database_name         = azurerm_cosmosdb_sql_database.contractnegotiation-store-db.name
-  partition_key_path    = "/partkey"
-  partition_key_version = 1
-  throughput            = 400
-
-  indexing_policy {
-    indexing_mode = "Consistent"
-
-    included_path {
-      path = "/*"
-    }
-
-    included_path {
-      path = "/included/?"
-    }
-
-    excluded_path {
-      path = "/excluded/?"
-    }
-  }
-
-}
 
 # Stored Procedures for Contract Negotiation Store
 resource "azurerm_cosmosdb_sql_stored_procedure" "nextForState-consumer" {
@@ -316,12 +233,21 @@ resource "azurerm_cosmosdb_sql_stored_procedure" "nextForState-provider" {
 
   body = file("nextForState.js")
 }
-resource "azurerm_cosmosdb_sql_stored_procedure" "nextForState-connector3" {
-  name                = "nextForState"
+resource "azurerm_cosmosdb_sql_stored_procedure" "lease-consumer" {
+  name                = "lease"
   resource_group_name = azurerm_cosmosdb_account.showcase-cosmos-account.resource_group_name
   account_name        = azurerm_cosmosdb_account.showcase-cosmos-account.name
   database_name       = azurerm_cosmosdb_sql_database.contractnegotiation-store-db.name
-  container_name      = azurerm_cosmosdb_sql_container.connector3-contractnegotiation-container.name
+  container_name      = azurerm_cosmosdb_sql_container.consumer-contractnegotiation-container.name
+  body                = file("lease.js")
+}
 
-  body = file("nextForState.js")
+resource "azurerm_cosmosdb_sql_stored_procedure" "lease-provider" {
+  name                = "lease"
+  resource_group_name = azurerm_cosmosdb_account.showcase-cosmos-account.resource_group_name
+  account_name        = azurerm_cosmosdb_account.showcase-cosmos-account.name
+  database_name       = azurerm_cosmosdb_sql_database.contractnegotiation-store-db.name
+  container_name      = azurerm_cosmosdb_sql_container.provider-contractnegotiation-container.name
+
+  body = file("lease.js")
 }
